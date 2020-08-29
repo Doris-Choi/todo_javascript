848b98267889bd5804a898fc366e5e973c22a0bd
doris <dychoi9298@gmail.com>
2020-08-30 02:02:06 +0900

    👉👈 이전달(prevMonth) 다음달(nextMonth) 클릭 이벤트 추가

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,12 +1,19 @@
-const point = new Date();
+let point = new Date();
 
-const monthYear = document.querySelector('.month-year');
-monthYear.innerText = `${point
-  .toString()
-  .substr(4, 3)
-  .toUpperCase()} ${point.getFullYear()}`;
+const prev = document.querySelector('.prev-month');
+const next = document.querySelector('.next-month');
+prev.addEventListener('click', prevMonth);
+next.addEventListener('click', nextMonth);
 
 function renderCalendar(point) {
+  // 달력의 월/년이 지정일에 따라 설정되도록
+  const monthYear = document.querySelector('.month-year');
+  monthYear.innerText = `${point
+    .toString()
+    .substr(4, 3)
+    .toUpperCase()} ${point.getFullYear()}`;
+
+  // 달력에 필요한 변수 선언
   const startMonth = new Date(point.getFullYear(), point.getMonth());
   const endMonth = new Date(point.getFullYear(), point.getMonth() + 1);
   endMonth.setDate(endMonth.getDate() - 1);
@@ -17,9 +24,9 @@ function renderCalendar(point) {
     endMonth.valueOf() + 1000 * 60 * 60 * 24 * (6 - endMonth.getDay()),
   );
 
+  // 달력 구조
   const days = document.createElement('div');
-  days.getAttribute('class', 'cal-wrapper');
-
+  days.classList.add('cal-wrapper');
   const thisDate = new Date(startDate);
   for (let i = 0; i < (endDate - startDate) / 24 / 60 / 60 / 1000 / 7; i++) {
     const weekBox = document.createElement('div');
@@ -40,8 +47,14 @@ function renderCalendar(point) {
       day.classList.toggle('sun', thisDate.getDay() === 0);
       day.classList.toggle('sat', thisDate.getDay() === 6);
 
-      weekBox.appendChild(day);
+      // date 정보를 남기기 위해
+      const dateInfo = document.createElement('div');
+      dateInfo.classList.add('date-info');
+      dateInfo.innerText = thisDate.toDateString();
+      dateInfo.style.cssText = 'text-indent:-9999px';
+      day.appendChild(dateInfo);
 
+      weekBox.appendChild(day);
       thisDate.setDate(thisDate.getDate() + 1);
     }
 
@@ -55,5 +68,19 @@ window.onload = () => {
   days.appendChild(renderCalendar(new Date()));
 };
 
-function prevMonth() {}
-function nextMonth() {}
+function prevMonth() {
+  const prevMonth = new Date(document.querySelector('.month-year').innerText);
+  prevMonth.setMonth(prevMonth.getMonth() - 1);
+
+  const days = document.querySelector('.days');
+  days.innerHTML = '';
+  days.appendChild(renderCalendar(prevMonth));
+}
+function nextMonth() {
+  const nextMonth = new Date(document.querySelector('.month-year').innerText);
+  nextMonth.setMonth(nextMonth.getMonth() + 1);
+
+  const days = document.querySelector('.days');
+  days.innerHTML = '';
+  days.appendChild(renderCalendar(nextMonth));
+}
